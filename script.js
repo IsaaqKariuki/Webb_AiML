88fcb39f52e47c5a8cd05f8cc22bdc50da0a73de
Isaaq <isaaqmwaura948@gmail.com>
2026-03-10 14:25:19 +0300

    session 1

---
+++ script.js
@@ -0,0 +1,12 @@
+
+ const button2= document.getElementById ("another1")
+   button2.addEventListener("click",function()
+       {
+         alert("Another button created fully functional 😝😎")
+      } )
+
+const button = document.getElementById("myButton")
+
+      button.addEventListener("click", function() {
+        alert("Wow i created my very first button🎉")
+      })
